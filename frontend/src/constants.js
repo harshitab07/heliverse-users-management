1a90acb01b96989685485d9eac0287e8e99a4335
harshitab07 <harshitabhatt1730@gmail.com>
2024-04-08 17:02:48 +0530

    API url removed from proxy

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = ''; 
+export const BASE_URL = 'https://heliverse-users-management.onrender.com'; 
 export const USERS_URL = '/api/users';
 export const TEAMS_URL = '/api/teams';
 export const UPLOADS_URL = '/api/upload';
